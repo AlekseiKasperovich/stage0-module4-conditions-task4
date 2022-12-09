c3ea21106eee6090a5709fb39474ae1ea93a9b42
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-12-09 16:57:09 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -1,6 +1,10 @@
 package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
+
     public void printGreatest(int first, int second, int third) {
+        int max = first >= second ? first : second;
+        int res = max >= third ? max : third;
+        System.out.println(res);
     }
 }
